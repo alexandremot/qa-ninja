ae6fa76c44a1802c54d4de2f4bb77907fbd59d0b
Alexandre Mota <alexandremot@gmail.com>
2020-04-16 01:57:39 -0300

    ajustes nos comentarios do arquivo drag and drop

--- capypara-zero-ao-ninja/spec/draganddrop_spec.rb
+++ capypara-zero-ao-ninja/spec/draganddrop_spec.rb
@@ -6,15 +6,19 @@ describe 'Drag and drop', :drag_and_drop do
     end
 
     it 'test' do
-        stark = find('.team-stark .column')
-        cap = find('.team-cap .column')
-
+        # localiza as respectivas divs do time stark e time cap
+        team_stark = find('.team-stark .column')
+        team_cap = find('.team-cap .column')
+        # localiza a imagem do homem aranha para selecionar (click)
         spiderman = find('img[alt$="Aranha"]')
         
-        spiderman.drag_to stark
-
-        expect(stark).to have_css 'img[alt$="Aranha"]'
-        expect(cap).not_to have_css 'img[alt$="Aranha"]'
+        # drag para o time stark
+        spiderman.drag_to team_stark
+        
+        # valida se a imagem foi posicionada no time stark
+        expect(team_stark).to have_css 'img[alt$="Aranha"]'
+        # valida se não contém a imagem no time cap
+        expect(team_cap).not_to have_css 'img[alt$="Aranha"]'
     end
 
     after(:each) do
