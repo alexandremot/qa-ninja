8a3140eb942e7b8f030c1706bb0e1e5a4efa9aac
Alexandre Mota <alexandremot@gmail.com>
2020-04-18 23:32:13 -0300

    adicionada suite para teste de envio de teclas

---
+++ capypara-zero-ao-ninja/spec/keys_spec.rb
@@ -0,0 +1,35 @@
+
+describe 'Keys', :keys do
+
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/key_presses'
+    end
+
+    it 'enviando teclas' do
+        # array com teclas para teste
+        teclas = %i[tab escape space enter shift control alt]
+
+        teclas.each do |tecla|
+            find('#campo-id').send_keys tecla
+            expect(page).to have_content 'You entered: ' + tecla.to_s.upcase
+            sleep 2
+        end
+    end
+
+
+    
+    it 'enviando letras' do
+        # array com strings para teste
+        letras = %w[a s d f g h j k l]
+
+        letras.each do |tecla|
+            find('#campo-id').send_keys tecla
+            expect(page).to have_content 'You entered: ' + tecla.to_s.upcase
+            sleep 2
+        end
+    end
+
+    after(:each) do
+        sleep 1
+    end
+end
